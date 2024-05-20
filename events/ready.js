e5a079f89ba1602b6a1eee38f852b91669578631
Stosbaby <57594354+Stosbaby@users.noreply.github.com>
2024-05-20 12:41:25 +0200

    Rebuild bot

--- events/ready.js
+++
@@ -1,9 +0,0 @@
-const { Events } = require('discord.js');
-
-module.exports = {
-	name: Events.ClientReady,
-	once: true,
-	execute(client) {
-		console.log(`Prêt ! Connecté en tant que ${client.user.tag}`);
-	},
-};
